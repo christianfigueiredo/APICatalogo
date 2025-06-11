21a41c7a0e279e2e315a396609d049b7cb1e0ec0
christian figueiredo <developer.figueiredo@gmail.com>
2025-06-11 08:23:13 -0300

    add method action get

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly Contexto _contexto;
+        public ProdutosController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _contexto.Produtos?.ToList();
+            if (produtos is null)
+            {
+                return NotFound();
+            }
+            return Ok(produtos);
+        }
+    }
+}
